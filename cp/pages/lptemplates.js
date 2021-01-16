e9dec13f861ebc0bb6692583e8f335062c687eef
duy ha <duyhph@gmail.com>
2021-01-16 17:36:06 +0700

    DEPLOY cp

--- cp/pages/lptemplates.js
+++ cp/pages/lptemplates.js
@@ -29,33 +29,24 @@ export default function Lptemplate() {
         return <></>
     } else {
         //=========== event handler:
-        const closeModal = () => {
-            setState({ ...state, file: {}, templatename: "", modal: false })
-        }
-        const submitModal = () => {
-            setState({ ...state, isLoading: true, nextAction: "create" })
-        }
-        const deleteTemplate = (tplName) => {
 
-            var name = prompt("Are you sure delete \"" + tplName + "\" template? Type the template name to delete", "");
-            name == tplName && setState({ ...state, isLoading: true, nextAction: "delete", payload: { name: tplName } })
-
+        const approveTemplate = (tplID) => {
+            setState({ ...state, isLoading: true, nextAction: "approve", payload: { ID: tplID } })
         }
-        const submitTemplate = (tplName) => {
-            setState({ ...state, isLoading: true, nextAction: "submit", payload: { name: tplName } })
-
+        const rejectTemplate = (tplID) => {
+            setState({ ...state, isLoading: true, nextAction: "reject", payload: { ID: tplID } })
         }
         //=========================
 
         if (state.isLoading) {
             switch (state.nextAction) {
                 case "get":
-                    GetData("lptpl",`lat`, userstate).then(rs => {
-                        console.log("data return", rs)
+                    GetData("lptpl", `lat`, userstate).then(rs => {
+
                         if (rs.Status === 1) {
                             try {
                                 const data = JSON.parse(rs.Data)
-                                setState({ ...state, isLoading: false, nextAction: "", Ptemplates: data.Ptemplates,Atemplates: data.Atemplates })
+                                setState({ ...state, isLoading: false, nextAction: "", Ptemplates: data.Ptemplates, Atemplates: data.Atemplates })
                                 return
                             } catch (e) {
                                 toast.error(e.message)
@@ -66,7 +57,55 @@ export default function Lptemplate() {
                         setState({ ...state, isLoading: false, nextAction: "" })
                     })
                     break;
-                
+                case "reject":
+                    GetData("lptpl", `rej|${state.payload.ID}`, userstate).then(rs => {
+                        if (rs.Status === 1) {
+                            try {
+                                //remove template
+                                const tpls = []
+                                for (let i = 0, n = state.Ptemplates.length; i < n; i++) {
+                                    if (state.Ptemplates[i].ID != state.payload.ID) {
+                                        tpls.push(state.Ptemplates[i])
+                                    }
+                                }
+                                setState({ ...state, isLoading: false, nextAction: "", Ptemplates: tpls })
+                                return
+                            } catch (e) {
+                                toast.error(e.message)
+                            }
+                        } else {
+                            toast.error(rs.Error)
+                        }
+                        setState({ ...state, isLoading: false, nextAction: "" })
+                    })
+                    break;
+                case "approve":
+                    GetData("lptpl", `ok|${state.payload.ID}`, userstate).then(rs => {
+                        if (rs.Status === 1) {
+                            try {
+                                //remove template
+                                const tpls = []
+                                var apprtpl = {}
+                                for (let i = 0, n = state.Ptemplates.length; i < n; i++) {
+                                    if (state.Ptemplates[i].ID == state.payload.ID) {
+                                        apprtpl = state.Ptemplates[i]
+                                    } else {
+                                        tpls.push(state.Ptemplates[i])
+                                    }
+                                }
+                                const apprtpls = state.Atemplates || []
+                                apprtpls.push(apprtpl)
+                                setState({ ...state, isLoading: false, nextAction: "", Ptemplates: tpls, Atemplates: apprtpls })
+                                return
+                            } catch (e) {
+                                toast.error(e.message)
+                            }
+                        } else {
+                            toast.error(rs.Error)
+                        }
+                        setState({ ...state, isLoading: false, nextAction: "" })
+                    })
+                    break;
 
                 default: setState({ ...state, nextAction: "", isLoading: false });
             }
@@ -85,54 +124,32 @@ export default function Lptemplate() {
 
                 <div className="row">
 
-
                     <div className="col-md-12 col-sm-12">
                         <div className="panel with-scroll animated zoomIn">
                             <div className="panel-heading clearfix">
                                 <div className="float-left"><h3 className="panel-title">Template Pending for test</h3></div>
-                                
+
                             </div>
                             <div className="panel-body text-center">
                                 <div className="ng-scope">
                                     {state.Ptemplates && state.Ptemplates.length > 0 && state.Ptemplates.map((tpl) =>
                                         <div key={tpl.Name} className="mx-5 my-12 float-left">
-                                            <div className="userpic" onClick={() => { window.location = process.env.NEXT_PUBLIC_TESTLP_URL+`test/edit/${encDat2(Cookies.get('_s')+"|"+tpl.Path)}` }}>
+                                            <div className="userpic" onClick={() => { window.location = process.env.NEXT_PUBLIC_TESTLP_URL + `test/edit/${encDat2(Cookies.get('_s') + "|" + tpl.Path)}` }}>
                                                 <div className="userpic-wrapper">
-                                                    <img src={process.env.NEXT_PUBLIC_TESTLP_URL+`templates/${tpl.Path}/screenshot.jpg`} />
+                                                    <img src={process.env.NEXT_PUBLIC_TESTLP_URL + `templates/${tpl.Path}/screenshot.jpg`} />
                                                 </div>
                                                 {tpl.Status == 0 &&
                                                     <i onClick={e => { e.stopPropagation(); deleteTemplate(tpl.Name) }} className="ion-ios-close-outline ng-scope"></i>
                                                 }
-                                                <a className="change-userpic" >{tpl.Name} </a>
+                                                <a className="change-userpic" >{tpl.Name} - {tpl.User} </a>
                                             </div>
-                                            {(tpl.Status == 0) &&
-                                                <button key={tpl.ID} type="button" onClick={() => submitTemplate(tpl.Name)} className="btn btn-default m-auto my-4">
-                                                    Submit
+
+                                            <button type="button" onClick={() => approveTemplate(tpl.ID)} className="btn btn-default m-auto my-4">
+                                                Approve
                       </button>
-                                            }
-                                            {(tpl.Status == -1) &&
-                                                <>
-                                                    <div className="py-2 text-red-500">{tpl.Description}</div>
-                                                    <button key={tpl.ID} type="button" onClick={() => submitTemplate(tpl.Name)} className="btn btn-default m-auto my-4">
-                                                        ReSubmit
+                                            <button type="button" onClick={() => rejectTemplate(tpl.ID)} className="btn btn-default m-auto my-4">
+                                                Reject
                       </button>
-                                                </>
-                                            }
-                                            {(tpl.Status == 2) &&
-                                                <button key={tpl.ID} type="button" disabled={true} className="btn btn-default m-auto my-4">
-                                                    Waiting approve
-                      </button>
-                                            }
-                                            {(tpl.Status == 1) &&
-                                                <>
-                                                    <div className="py-2">
-                                                        Viewed: {tpl.Viewed} - Installed: {tpl.Installed}
-                                                    </div>
-                                                    <button key={tpl.ID} type="button" disabled="true" className="btn btn-default m-auto my-4">
-                                                        Approve
-                      </button>
-                                                </>
-                                            }
 
                                         </div>
                                     )}
@@ -140,9 +157,41 @@ export default function Lptemplate() {
                             </div>
                         </div>
                     </div>
+
+
+                    <div className="col-md-12 col-sm-12">
+                        <div className="panel with-scroll animated zoomIn">
+                            <div className="panel-heading clearfix">
+                                <div className="float-left"><h3 className="panel-title">Template Approved</h3></div>
+
+                            </div>
+                            <div className="panel-body text-center">
+                                <div className="ng-scope">
+                                    {state.Atemplates && state.Atemplates.length > 0 && state.Atemplates.map((tpl) =>
+                                        <div key={tpl.Name} className="mx-5 my-12 float-left">
+                                            <div className="userpic" onClick={() => { window.location = process.env.NEXT_PUBLIC_TESTLP_URL + `test/edit/${encDat2(Cookies.get('_s') + "|" + tpl.Path)}` }}>
+                                                <div className="userpic-wrapper">
+                                                    <img src={process.env.NEXT_PUBLIC_TESTLP_URL + `templates/${tpl.Path}/screenshot.jpg`} />
+                                                </div>
+                                                {tpl.Status == 0 &&
+                                                    <i onClick={e => { e.stopPropagation(); deleteTemplate(tpl.Name) }} className="ion-ios-close-outline ng-scope"></i>
+                                                }
+                                                <a className="change-userpic" >{tpl.Name} - {tpl.User} </a>
+                                            </div>
+
+                                            <div className="py-2 text-red-500">Viewed:{tpl.Viewed} - Installed:{tpl.Installed}</div>
+
+                                        </div>
+                                    )}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
                 </div>
 
-                
+
             </Layout>
         )
 
