da2a1d28ae056b17cb2c32e75bc5819f1856fb0b
duy ha <duyhph@gmail.com>
2021-02-04 12:03:38 +0700

    DEPLOY lpmin output folder and data

--- lpmin/api/controllers/purgecss.js
+++ lpmin/api/controllers/purgecss.js
@@ -22,7 +22,7 @@ exports.purge = async function (req, res) {
         const encryptedsex = req.body.data && crypto.decDat(req.body.data, 3) || ""
 
         rs = await mydata.GetData("aut", "t", encryptedsex)
-        
+
 
         if (rs.Status == 1) {
             try {
@@ -45,20 +45,27 @@ exports.purge = async function (req, res) {
         if (rs.Status == 1) {
             const buildFolder = `templates/${tplname}/`
             const outFolder = `templates/${tplname}/out/`
-            if (!fs.existsSync(buildFolder+"build/")) {
+            if (!fs.existsSync(buildFolder)) {
                 rs.Status = 0
                 rs.Error = "template build folder not exist"
+            } if (!fs.existsSync(outFolder)) {
+                rs.Status = 0
+                rs.Error = "template output folder not exist"
             } else {
-                //creat output folder
-                fs.rmdirSync(outFolder, { recursive: true })
-                fs.mkdirSync(outFolder)
-                await build(buildFolder, outFolder,buildFolder + 'build/content.html')
-
+                //creat output folder             
+                var builddata = { 
+                    BuildFolder: buildFolder, 
+                    OutFolder: outFolder,                     
+                    OrgID: "test", 
+                    CampID: "test", 
+                    Favicon: ""
+                 }
+                await build(builddata)
             }
         }
         if (rs.Status == 0) rs.Data = ""
     } catch (e) {
-        rs.Status=0;
+        rs.Status = 0;
         rs.Error = e.message
     }
 
@@ -70,18 +77,22 @@ exports.purge = async function (req, res) {
 exports.publish = async function (req, res) {
     var rs = { Error: "", Data: "", Status: 0, Message: "" }
     try {
-        
+
         const tplpath = req.params.params && crypto.decDat(req.params.params, 3) || ""
-        const args = (req.body.data && crypto.decDat(req.body.data, 3) || "").split(",")
-        if (args.length < 3) {
-            rs.Error = "Invalid params"
-            res.send(crypto.encDat2(JSON.stringify(rs)))            
+        const args = (req.body.data && crypto.decDat(req.body.data, 3) || "")
+        var builddata={}
+        try{
+            builddata=JSON.parse(args)
+        } catch(e){
+            rs.Error = "Invalid params:"+e.message;
+            res.send(crypto.encDat2(JSON.stringify(rs)))
             return
         }
-        const sex = args[0]
-        const lppath = args[1]
-        const favicon=crypto.base64decode(args[2])
-        rs = await mydata.GetData("aut", "t",crypto.encDat2(sex))
+        const sex = builddata.Session        
+        builddata.BuildFolder=`templates/${tplpath}/`
+        builddata.OutFolder=`templates/${tplpath}/publish/${builddata.LPPath}/`
+        
+        rs = await mydata.GetData("aut", "t", crypto.encDat2(sex))
         if (rs.Status == 1) {
             try {
                 const data = JSON.parse(rs.Data)
@@ -100,20 +111,20 @@ exports.publish = async function (req, res) {
             }
         }
         if (rs.Status == 1) {
-            const buildFolder = `templates/${tplpath}/`
-            const outFolder = `templates/${tplpath}/publish/${lppath}/`
-
-            if (!fs.existsSync(buildFolder+"build/")) {
+            if (!fs.existsSync(builddata.buildFolder )) {
                 rs.Status = 0
                 rs.Error = "template build folder not exist"
+            }if (!fs.existsSync(builddata.outFolder)) {
+                rs.Status = 0
+                rs.Error = "landingpage output folder not exist"
             } else {
-                await build(buildFolder, outFolder,outFolder + 'content.html',favicon)
+                await build(builddata)
                 console.log("waiting")
             }
         }
         if (rs.Status == 0) rs.Data = ""
     } catch (e) {
-        rs.Status=0
+        rs.Status = 0
         rs.Error = e.message
     }
     console.log(rs)
@@ -121,11 +132,12 @@ exports.publish = async function (req, res) {
 
 };
 
-async function build(buildFolder, outFolder,contentfile,favicon) {
+async function build(builddata) {
     //remove unused css
+    const contentfile=builddata.OutFolder+"content.html";
     const purgeCSSResults = await new purgecss.PurgeCSS().purge({
-        content: [contentfile, buildFolder + 'js/*.js'],
-        css: [buildFolder + 'css/*.css'],
+        content: [contentfile, builddata.BuildFolder + 'js/*.js'],
+        css: [builddata.BuildFolder + 'css/*.css'],
         extractors: [
             {
                 extractor: content => {
@@ -137,18 +149,29 @@ async function build(buildFolder, outFolder,contentfile,favicon) {
         ]
     })
 
-    var csscontent=""
+    var csscontent = ""
     purgeCSSResults.forEach(item => {
-        csscontent+=item.css
+        csscontent += item.css
     })
 
-    csscontent=csscontent.replace(/\\:/g, "--").replace(/\\\//g, "-div-")
-    csscontent=cs.replaceCss(csscontent)
-    
+    csscontent = csscontent.replace(/\\:/g, "--").replace(/\\\//g, "-div-")
+    csscontent = cs.replaceCss(csscontent)
+
     //minify css                    
-    fs.writeFileSync(outFolder + 'style.css', csso.minify(csscontent).css, { mode: '655' });
+    fs.writeFileSync(builddata.OutFolder + 'style.css', csso.minify(csscontent).css, { mode: '655' });
     //minify js
+    builddata.SuccessTitle=builddata.SuccessTitle&&builddata.SuccessTitle.trim()!=""?builddata.SuccessTitle.trim():"Success"
+    builddata.SuccessMessage=builddata.SuccessMessage&&builddata.SuccessMessage.trim()!=""?builddata.SuccessMessage.trim():"Data sent. Thank you."
+    builddata.ErrorTitle=builddata.ErrorTitle&&builddata.ErrorTitle.trim()!=""?builddata.ErrorTitle.trim():"Error"
+    builddata.ErrorMessage=builddata.ErrorMessage&&builddata.ErrorMessage.trim()!=""?builddata.ErrorMessage.trim():"Something wrong. "
+
+    var orgID = builddata.OrgID || "test"
+    if (orgID != "") orgID = `var orgID="${orgID}";`
+    var campID = builddata.CampID || "test"
+    if (campID != "") campID = `var campID="${campID}";`
     var jsmin = `
+    ${orgID}
+    ${campID}
     var caesarShift = function (str, amount) {
         // Wrap the amount
         if (amount < 0) {
@@ -334,24 +357,6 @@ async function build(buildFolder, outFolder,contentfile,favicon) {
         
             return Promise.resolve(rs);
         }
-
-        function serverSubmit(){
-            const data=JSON.stringify({name:document.querySelector("#name"),phone:document.querySelector("#phone"),email:document.querySelector("#email"),message:document.querySelector("#message")})
-            GetData("lead","s|"+base64encode(data)).then(rs => {
-                console.log("data return", rs)
-                if (rs.Status === 1) {
-                  try {
-                    
-                  } catch (e) {
-                    showMessage("error",e.message,"error");
-                  }
-                } else {
-                    showMessage("error",rs.Error,"error");
-                }
-                
-            })
-        }
-
         window.onload = function(e) {
             GetData("i",getStorage("_x")).then(rs => {
                 console.log("data return", rs)
@@ -362,29 +367,43 @@ async function build(buildFolder, outFolder,contentfile,favicon) {
                     console.log(rs.Error)
                 }
                 
-            })
+            })            
+            if(serverWindowLoad)serverWindowLoad(e);
         }
-
       `
-    const files = fs.readdirSync(buildFolder+"js/")
+    const files = fs.readdirSync(builddata.BuildFolder + "js/")
     for (let i = 0, n = files.length; i < n; i++) {
         const file = files[i]
         if (file.substr(file.length - 3, 3) == ".js") {
-            jsmin += fs.readFileSync(buildFolder + "js/" + file, "utf8")
+            jsmin += fs.readFileSync(builddata.BuildFolder + "js/" + file, "utf8")
         }
     }
+
+    //override server function:
+    jsmin+=`function serverSubmit(){
+        const data=JSON.stringify({OrgID:orgID,CampaignID:campID,Name:document.querySelector("#name").value,Phone:document.querySelector("#phone").value,Email:document.querySelector("#email").value,message:document.querySelector("#message").value})
+        GetData("lpl","s|"+base64encode(data)).then(rs => {
+            if (rs.Status === 1) {                
+                showMessage("${builddata.SuccessTitle}","${builddata.SuccessMessage}","success")
+              
+            } else {
+                showMessage("${builddata.ErrorTitle}","${builddata.ErrorMessage}"+rs.Error,"error");
+            }
+            
+        })
+    }`
 
     //shorten cssmin in js
     let regexp = /(classList\.\w+|getElementsByClassName|hasClass|addClass|removeClass)\(["'](.*?)["']\)/g;
     const cssmap = cs.getMap()
-    
+
     jsmin = jsmin.replace(regexp, (match, g1, g2) => {
 
         return cssmap[g2] ? g1 + `("${cssmap[g2]}")` : match
     })
     regexp = /(querySelector\(["'].*?\.|querySelectorAll\(["'].*?\.|className=["']|\.attr\(["']class["'],["']|setAttribute\(["']class["'],["'])(.*?)(["'])/g;
     jsmin = jsmin.replace(regexp, (match, g1, g2, g3) => {
-        const rs = [];                
+        const rs = [];
         g2.split(" ").forEach(item => {
             cssmap[item] ? rs.push(cssmap[item]) : rs.push(item)
         })
@@ -396,51 +415,51 @@ async function build(buildFolder, outFolder,contentfile,favicon) {
         //     url: "out.js.map"
         // },
         mangle: {
-            toplevel: true,            
+            toplevel: true,
         },
         nameCache: {}
-    };    
-    jsmin = UglifyJS.minify(jsmin,options).code
+    };
+    jsmin = UglifyJS.minify(jsmin, options).code
 
-    
-    contentfile=fs.readFileSync(contentfile, "utf8")
+
+    var lpcontent = fs.readFileSync(contentfile, "utf8")
     //remove html trash and contenteditable property
-    contentfile=contentfile.replace(/contenteditable=\"true\"/g,"")
-    contentfile=contentfile.replace(/<div class=\"landingpage-trash.*?".*?<\/div>.*?<\/div>/gms,"")
+    lpcontent = lpcontent.replace(/contenteditable=\"true\"/g, "")
+    lpcontent = lpcontent.replace(/<div class=\"landingpage-trash.*?".*?<\/div>.*?<\/div>/gms, "")
 
     //replace tailwindcss special char in html
     regexp = /(class=['"])(.*?)(['"])/gms;
-    contentfile = contentfile.replace(regexp, (match, g1, g2, g3) => {
-        return g1+g2.replace(/:/g, "--").replace(/\//g, "-div-")+g3
+    lpcontent = lpcontent.replace(regexp, (match, g1, g2, g3) => {
+        return g1 + g2.replace(/:/g, "--").replace(/\//g, "-div-") + g3
     })
-    contentfile=cs.replaceHtml(contentfile)
+    lpcontent = cs.replaceHtml(lpcontent)
     //remove class not declare in css (classname len <4)
-    contentfile = contentfile.replace(regexp, (match, g1, g2, g3) => {
+    lpcontent = lpcontent.replace(regexp, (match, g1, g2, g3) => {
         const lsClass = g2.split(" ")
         const rsClass = []
         for (let i = 0, n = lsClass.length; i < n; i++) {
             if (lsClass[i].length < 4) rsClass.push(lsClass[i])
         }
-        return g1+rsClass.join(" ")+g3
+        return g1 + rsClass.join(" ") + g3
     })
-    var faviconmeta=``
-    if(favicon&&favicon.trim()!=""){
-        var icontype=`image/x-icon`
-        var iconext=favicon.substr(favicon.indexOf("."))
-        if(iconext==".gif"){
-            icontype=`image/gif`
-        }else if(iconext==".png"){
-            icontype=`image/png`
-        }else if(iconext==".jpeg"||iconext==".jpg"){
-            icontype=`image/jpeg`
-        }else if(iconext==".webp"){
-            icontype=`image/webp`
+    var faviconmeta = ``
+    if (builddata.Favicon && builddata.Favicon.trim() != "") {
+        var icontype = `image/x-icon`
+        var iconext = builddata.Favicon.substr(builddata.Favicon.indexOf("."))
+        if (iconext == ".gif") {
+            icontype = `image/gif`
+        } else if (iconext == ".png") {
+            icontype = `image/png`
+        } else if (iconext == ".jpeg" || iconext == ".jpg") {
+            icontype = `image/jpeg`
+        } else if (iconext == ".webp") {
+            icontype = `image/webp`
         }
-        faviconmeta=`<link rel="icon" href="${favicon}" type="${icontype}" /><link rel="shortcut icon" href="${favicon}" type="${icontype}" />`
+        faviconmeta = `<link rel="icon" href="${builddata.Favicon}" type="${icontype}" /><link rel="shortcut icon" href="${favicon}" type="${icontype}" />`
     }
 
 
-    var htmlcontent=`<!DOCTYPE html>
+    var htmlcontent = `<!DOCTYPE html>
     <html lang="en">
     <head>
     <meta charset="UTF-8">
@@ -449,91 +468,91 @@ async function build(buildFolder, outFolder,contentfile,favicon) {
     <title>Landing page test</title>
     <link href="style.css" rel="stylesheet">
     </head>
-    ${contentfile}
+    ${lpcontent}
     <script>${jsmin}</script></body></html>
-    `    
-    
+    `
+
     htmlcontent = htmlmin.minify(htmlcontent, {
-                        collapseWhitespace: true,
-                        removeEmptyAttributes: true,
-                        removeComments: true,
-                        removeTagWhitespace: true
-                    })
-    fs.writeFileSync(outFolder + 'index.html', htmlcontent, { mode: '655' });
-        
+        collapseWhitespace: true,
+        removeEmptyAttributes: true,
+        removeComments: true,
+        removeTagWhitespace: true
+    })
+    fs.writeFileSync(builddata.OutFolder + 'index.html', htmlcontent, { mode: '655' });
 
 
 
-//     //shorten css
-//     let bl = new BufferListStream()
-//     let bl2 = new BufferListStream()
-//     purgeCSSResults.forEach(item => {
-//         bl.append(Buffer.from(item.css))
-//     })
 
-//     bl.pipe(strStream(/(\\:)|(\\\/)/g, function (str) {
-//         return str.replace("\\:", "--").replace(`\\\/`, "-div-")
-//     }
-//     ))
-//         .pipe(cs.cssStream())
-//         .on('data', (data) => { bl2.append(data) })
-//         .on('end', () => {
-            
-//             // bl = new BufferListStream()
-//             // var jscssmin=``
-//             // bl.append(jsmin)
-//             // bl.pipe(cs.htmlStream())
-//             // .on('data', (data) => {
-//             //     jscssmin+=data.toString()
-//             // })
-//             // .on('end',()=>{
-//             //     console.log(jscssmin)
-//             // })
+    //     //shorten css
+    //     let bl = new BufferListStream()
+    //     let bl2 = new BufferListStream()
+    //     purgeCSSResults.forEach(item => {
+    //         bl.append(Buffer.from(item.css))
+    //     })
 
-//             //shorten css in html           
-//             bl = new BufferListStream()
-//             bl.append(`<!DOCTYPE html>
-// <html lang="en">
-// <head>
-// <meta charset="UTF-8">
-// <meta name="viewport" content="width=device-width">
-// <title>Landing page test</title>
-// <link href="style.css" rel="stylesheet">
-// </head>
-// `)
+    //     bl.pipe(strStream(/(\\:)|(\\\/)/g, function (str) {
+    //         return str.replace("\\:", "--").replace(`\\\/`, "-div-")
+    //     }
+    //     ))
+    //         .pipe(cs.cssStream())
+    //         .on('data', (data) => { bl2.append(data) })
+    //         .on('end', () => {
 
-//             fs.createReadStream(contentfile)
-//                 .pipe(strStream(/class="(.*?)"/g, function (str) {
-//                     return str.replace(/:/g, "--").replace(/\//g, "-div-")
-//                 }))
-//                 .pipe(cs.htmlStream())
-//                 //remove class not declare in css (classname len <4)
-//                 .pipe(strStream(/class="(.*?)"/g, function (str, matched) {
-//                     const lsClass = matched.split(" ")
-//                     const rsClass = []
-//                     for (let i = 0, n = lsClass.length; i < n; i++) {
-//                         if (lsClass[i].length < 4) rsClass.push(lsClass[i])
-//                     }
-//                     return `class="${rsClass.join(" ")}"`
+    //             // bl = new BufferListStream()
+    //             // var jscssmin=``
+    //             // bl.append(jsmin)
+    //             // bl.pipe(cs.htmlStream())
+    //             // .on('data', (data) => {
+    //             //     jscssmin+=data.toString()
+    //             // })
+    //             // .on('end',()=>{
+    //             //     console.log(jscssmin)
+    //             // })
 
-//                 })).on('data', (data) => {
-//                     bl.append(data.toString())
-//                 }).on('end', () => {
+    //             //shorten css in html           
+    //             bl = new BufferListStream()
+    //             bl.append(`<!DOCTYPE html>
+    // <html lang="en">
+    // <head>
+    // <meta charset="UTF-8">
+    // <meta name="viewport" content="width=device-width">
+    // <title>Landing page test</title>
+    // <link href="style.css" rel="stylesheet">
+    // </head>
+    // `)
 
-//                     //minify js
-//                     bl.append(`<script>`)
-//                     bl.append(jsmin)
+    //             fs.createReadStream(contentfile)
+    //                 .pipe(strStream(/class="(.*?)"/g, function (str) {
+    //                     return str.replace(/:/g, "--").replace(/\//g, "-div-")
+    //                 }))
+    //                 .pipe(cs.htmlStream())
+    //                 //remove class not declare in css (classname len <4)
+    //                 .pipe(strStream(/class="(.*?)"/g, function (str, matched) {
+    //                     const lsClass = matched.split(" ")
+    //                     const rsClass = []
+    //                     for (let i = 0, n = lsClass.length; i < n; i++) {
+    //                         if (lsClass[i].length < 4) rsClass.push(lsClass[i])
+    //                     }
+    //                     return `class="${rsClass.join(" ")}"`
 
-//                     bl.append(`</script></body></html>`)
+    //                 })).on('data', (data) => {
+    //                     bl.append(data.toString())
+    //                 }).on('end', () => {
 
-//                     const html = htmlmin.minify(bl.toString(), {
-//                         collapseWhitespace: true,
-//                         removeEmptyAttributes: true,
-//                         removeComments: true,
-//                         removeTagWhitespace: true
-//                     })
-//                     fs.writeFileSync(outFolder + 'index.html', html, { mode: '655' });
-//                 })
-//         })
+    //                     //minify js
+    //                     bl.append(`<script>`)
+    //                     bl.append(jsmin)
+
+    //                     bl.append(`</script></body></html>`)
+
+    //                     const html = htmlmin.minify(bl.toString(), {
+    //                         collapseWhitespace: true,
+    //                         removeEmptyAttributes: true,
+    //                         removeComments: true,
+    //                         removeTagWhitespace: true
+    //                     })
+    //                     fs.writeFileSync(outFolder + 'index.html', html, { mode: '655' });
+    //                 })
+    //         })
 
 }
